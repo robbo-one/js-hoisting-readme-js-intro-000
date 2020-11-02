15331fcae8063ae9fb63d2721dd2507b271c347c
R C <robsc801@gmail.com>
2020-11-02 23:30:42 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,12 +1,12 @@
-var maybe = function(){
-var maybe = "maybe";
-return maybe;
+function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  console.log (maybe());
-
+  console.log("maybe");
+  return lyric;
+  
 }
 function crazy() {
   // fix the code in here:
