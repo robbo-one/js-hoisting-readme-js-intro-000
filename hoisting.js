7b36397043854c37d2bee23788974cb636d7f2cd
R C <robsc801@gmail.com>
2020-11-02 22:59:21 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,9 @@
-function callMe() {
-  var lyric = "maybe";
+var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  function callMe()
   return lyric;
 }
 function crazy() {
